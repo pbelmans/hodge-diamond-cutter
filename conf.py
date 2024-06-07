38a6dbfc3e75f1bb4375c69ba88da32943cc5a0d
Pieter Belmans <pieterbelmans@gmail.com>
2024-06-07 10:56:15 +0200

    docs: add Plausible analytics and change theme

--- conf.py
+++ conf.py
@@ -67,7 +67,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = "sphinx_book_theme"
 html_logo = "logo.png"
 html_favicon = "favicon.ico"
 
