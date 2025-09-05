d0bbc3560834ed5465845c19b47f7cf21c2f2880
thilobaumann <baumann.thilo@proton.me>
2025-09-05 13:54:34 +0200

    feat: code for entries of s-tuples

--- diamond/diamond.py
+++ diamond/diamond.py
@@ -3316,12 +3316,29 @@ def BrauerSeveri(g, n, Ns):
         sage: BrauerSeveri(0, 2, [(1, 1)]) == Pn(2).blowup(2 * point())
         True
 
+    Example 2.3.3 in Baumann's thesis:
+
+        sage: from diamond import *
+        sage: N = [3,1,4,2]
+        sage: s1 = [2,6,7,10]
+        sage: s(N,1,1) == s1[0]
+        True
+        sage: s(N,1,2) == s1[1]
+        True
+        sage: s(N,1,3) == s1[2]
+        True
+        sage: s(N,1,4) == s1[3]
+        True
     """
 
     def s(N, i, j):
         r"""Equation (2.15) in Baumann's thesis"""
-        # TODO implement this
-        pass
+        assert i in range(1, len(N) + 1)
+        assert j in range(1, len(N) + 1)
+        r"""Use formula for s after Equation (2.17) in Baumann's thesis"""
+        N_permuted = N[i - 1 :] + N[: i - 1]
+        r"""Sum of last j entries in N_permuted"""
+        return sum(N_permuted[k] for k in range(len(N) - j, len(N)))
 
     def m(i, N, k):
         r"""Line following Equation (2.16) in Baumann's thesis"""
